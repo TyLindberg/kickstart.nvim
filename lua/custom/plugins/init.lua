314326b6477abb00d291f41cddd17acedcc73354
Tyler Lindberg <tylindberg@hotmail.com>
2024-05-31 11:43:13 -0700

    Add nvim-dap and nvim-dap-python

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -40,4 +40,61 @@ return {
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
     end,
   },
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'theHamsta/nvim-dap-virtual-text',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
+      'mfussenegger/nvim-dap-python',
+    },
+    config = function()
+      local dap = require 'dap'
+      local ui = require 'dapui'
+
+      require('dapui').setup()
+      require('nvim-dap-virtual-text').setup()
+      require('dap-python').setup '~/.virtualenvs/debugpy/bin/python'
+
+      require('dap.ext.vscode').load_launchjs()
+
+      vim.keymap.set('n', '<space>db', dap.toggle_breakpoint, { desc = 'Toggle [B]reakpoint' })
+      vim.keymap.set('n', '<space>dc', dap.run_to_cursor, { desc = 'Run to [C]ursor' })
+
+      -- Eval var under cursor
+      vim.keymap.set('n', '<space>d?', function()
+        require('dapui').eval(nil, { enter = true })
+      end, { desc = 'Eval Under Cursor' })
+
+      vim.keymap.set('n', '<leader>d<space>', dap.continue, { desc = 'Continue' })
+      vim.keymap.set('n', '<leader>dl', dap.step_into, { desc = 'Step Into' })
+      vim.keymap.set('n', '<leader>dj', dap.step_over, { desc = 'Step Over' })
+      vim.keymap.set('n', '<leader>dh', dap.step_out, { desc = 'Step Out' })
+      vim.keymap.set('n', '<leader>dk', dap.step_back, { desc = 'Step Back' })
+      vim.keymap.set('n', '<leader>dr', dap.restart, { desc = '[R]estart' })
+
+      vim.keymap.set('n', '<leader>dd', dap.disconnect, { desc = '[D]isconnect' })
+
+      -- vim.keymap.set('n', '<F1>', dap.continue)
+      -- vim.keymap.set('n', '<F2>', dap.step_into)
+      -- vim.keymap.set('n', '<F3>', dap.step_over)
+      -- vim.keymap.set('n', '<F4>', dap.step_out)
+      -- vim.keymap.set('n', '<F5>', dap.step_back)
+      -- vim.keymap.set('n', '<F13>', dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
 }
